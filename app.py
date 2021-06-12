a16c959ffb019dd065c762b33ae7da9b459c00ea
Jeff Blosser <jblosser@therestaurantstore.com>
2021-06-12 17:58:00 -0400

    remove commented out code

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    # Load current count
-    # f = open("count.txt", "r")
     x = open('prefix.txt', 'r')
     y = open('first.txt', 'r')
     z = open('second.txt', 'r')
@@ -36,21 +34,10 @@ def index():
     # f = f[:-2]
 
     result = d + e + f
-    # count = int(f.read())
-    # f.close()
 
-    # Increment the count
-    # count += 1
-
-    # Overwrite the count
-    # f = open("count.txt", "w")
-    # f.write(str(count))
-    # f.close()
     x.close()
     y.close()
     z.close()
-
-    # Render HTML with count variable
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
