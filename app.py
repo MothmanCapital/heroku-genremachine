9e8a0541e104310e1ec547ff819713b134dc2524
Jeff Blosser <jblosser@therestaurantstore.com>
2021-06-13 11:32:46 -0400

    refactor logic, add lots of words

--- app.py
+++ app.py
@@ -1,44 +1,66 @@
 import random
 from flask import Flask, render_template
+from difflib import SequenceMatcher
+
+a = []
+b = []
+c = []
+
+wordList = []
+phraseLength = 3
+
+x = open('prefix.txt', 'r')
+y = open('first.txt', 'r')
+z = open('second.txt', 'r')
+
+for line in x:
+  if len(line) > 0:
+    a.append(line)
+for line in y:
+  if len(line) > 0:
+    b.append(line)
+for line in z:
+  if len(line) > 0:
+    c.append(line)
+
+wordList.append(a)
+wordList.append(b)
+wordList.append(c)
+
+x.close()
+y.close()
+z.close()
+
+def getWord(wordList, arr, idx):
+  randomWord = str(random.choice(wordList))
+  if idx > 0:
+    s = SequenceMatcher(None, arr[idx-1], randomWord)
+    if s.ratio() < 0.8:
+      return randomWord
+    else:
+      getWord(wordList, arr, idx)
+  else:
+    return randomWord
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 
-    x = open('prefix.txt', 'r')
-    y = open('first.txt', 'r')
-    z = open('second.txt', 'r')
+  phrase = []
+  finalPhrase = ""
+  for phraseIdx in range(phraseLength):
+    phrase.append(getWord(wordList[phraseIdx], phrase, phraseIdx).rstrip())
 
-    a = []
-    b = []
-    c = []
+  for phraseIdx in range(phraseLength):
+    if not(phrase[phraseIdx].endswith('-')):
+      finalPhrase += phrase[phraseIdx] + " "
+    else:
+      finalPhrase += phrase[phraseIdx]
 
-    for line in x:
-      a.append(line)
+  result = finalPhrase
 
-    for line in y:
-      b.append(line)
-
-    for line in z:
-      c.append(line)
-
-    d = str(random.choice(a))
-    # d = d[:-2]
-    d += ' '
-    e = str(random.choice(b))
-    e = e[:-1]
-    if not(e.endswith('-')):
-      e += ' '
-    f = str(random.choice(c))
-    # f = f[:-2]
-
-    result = d + e + f
-
-    x.close()
-    y.close()
-    z.close()
-    return render_template("index.html", result=result)
+  return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run()
