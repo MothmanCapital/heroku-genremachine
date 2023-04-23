451517e7ce24dabfd0bfe3f9a708561690e29307
Jeff Blosser <jeff.n.blosser@gmail.com>
2023-04-23 16:14:49 -0700

    saving works, working on retrieving

--- UrlShare.py
+++ UrlShare.py
@@ -5,57 +5,56 @@ import shortuuid
 from time import time
 from google.cloud import firestore
 
+
 class UrlShare(object):
-  def __init__(self, phrase, img_url, search_term, clicks=0):
-    self.uuid = shortuuid.uuid()[:10]
-    self.created = int(time())
-    self.phrase = phrase
-    self.img_url = img_url
-    self.search_term = search_term
-    self.clicks = clicks
+    def __init__(self, phrase, img_url, search_term, clicks=0):
+        self.uuid = shortuuid.uuid()[:10]
+        self.created = int(time())
+        self.phrase = phrase
+        self.img_url = img_url
+        self.search_term = search_term
+        self.clicks = clicks
 
-    # Initialize Firebase connection
-    self.db = firestore.Client(project='genremachine')
-    self.collection = self.db.collection(u'shared_urls')
-    if self.check_duplicate():
-      self.commit()
-      print("Saved link")
-    else:
-      print("picture already in use. Did not save")
+        # Initialize Firebase connection
+        self.db = firestore.Client(project="genremachine-2e8a4")
+        self.collection = self.db.collection("shared_urls")
+        if self.check_duplicate():
+            self.commit()
+            print("Saved link")
+        else:
+            print("picture already in use. Did not save")
 
-  def add_click(self):
-    self.clicks += 1
-    self.commit()
+    def add_click(self):
+        self.clicks += 1
+        self.commit()
 
-  def check_duplicate(self):
-    duplicate_query = self.collection.where(u'img', u'==', self.img_url).stream()
-    if len(duplicate_query) > 0:
-      for q in duplicate_query:
-        print(f'duplicate: {q.id} => {q.to_dict()}')
-      return False
-    else:
-      return True
+    def check_duplicate(self):
+        duplicate_query = self.collection.where("img", "==", self.img_url).stream()
+        for i in duplicate_query:
+            if i:
+                print(f"duplicate: {q.id} => {q.to_dict()}")
+            return False
+        else:
+            return True
 
-  def commit(self):
-    self.collection.document(self.uuid).set(
-      {
-        "phrase":   self.phrase,
-        "img":      self.img_url,
-        "search":   self.search_term,
-        "created":  self.created,
-        "clicks":   self.clicks
-      }
-    )
-    return True
+    def commit(self):
+        self.collection.document(self.uuid).set(
+            {
+                "phrase": self.phrase,
+                "img": self.img_url,
+                "search": self.search_term,
+                "created": self.created,
+                "clicks": self.clicks,
+            }
+        )
+        return True
 
-  def __repr__(self):
-    return (
-      f'UrlShare(\
-        id={self.id}, \
+    def __repr__(self):
+        return f"UrlShare(\
+        uuid={self.uuid}, \
         created={self.created}, \
         phrase={self.phrase}, \
         search_term={self.search_term}, \
         img_url={self.img_url}, \
         clicks={self.clicks}\
-      )'
-    )
+      )"
--- app.py
+++ app.py
@@ -15,195 +15,215 @@ from difflib import SequenceMatcher
 
 from UrlShare import UrlShare
 
-with httpimport.github_repo('MothmanCapital', 'pyunsplash', branch = 'add-support-for-embed-urls'):
+with httpimport.github_repo(
+    "MothmanCapital", "pyunsplash", branch="add-support-for-embed-urls"
+):
     from pyunsplash import PyUnsplash
 
 # initialize firestore connection
 cred = credentials.Certificate("genremachine.json")
 firebase_admin.initialize_app(cred)
 
-client_id = os.environ['UNSPLASH_ID']
-client_secret = os.environ['UNSPLASH_KEY']
-redirect_uri = os.environ['UNSPLASH_REDIR']
+client_id = os.environ["UNSPLASH_ID"]
+client_secret = os.environ["UNSPLASH_KEY"]
+redirect_uri = os.environ["UNSPLASH_REDIR"]
 
 # Initialize PyUnsplash app logging
 logger = logging.getLogger()
-logging.basicConfig(filename='app.log', level=logging.DEBUG)
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
 logging.getLogger(PyUnsplash.logger_name).setLevel(logging.DEBUG)
 
 wordsCombined = []
 exclusiveTags = []
 phraseLength = 3
 
+
 def readFiles(filename, listToReadInto):
-  fileHolder = open(filename, 'r')
-  for line in fileHolder:
-    if line.strip() and (line != None) and (line[0] != "#"):
-      listToReadInto.append(line.strip())
-  return listToReadInto
-  fileHolder.close()
+    fileHolder = open(filename, "r")
+    for line in fileHolder:
+        if line.strip() and (line != None) and (line[0] != "#"):
+            listToReadInto.append(line.strip())
+    return listToReadInto
+    fileHolder.close()
+
 
 # Could be moved into the main app route to refresh word list live instead of having to restart the app
-readFiles('combined.txt', wordsCombined)
-readFiles('exclusive-tags.txt', exclusiveTags)
+readFiles("combined.txt", wordsCombined)
+readFiles("exclusive-tags.txt", exclusiveTags)
+
 
 def processWordList(listToProcess):
-  wordStorage = []
-  for w in listToProcess:
-    wordTemp = []
-    w = w.strip()
-    wordTemp.append(w.split(';')[0])
-    wordTemp.append(w.split(';')[1].split(','))
-    wordStorage.append(wordTemp)
-  return wordStorage
+    wordStorage = []
+    for w in listToProcess:
+        wordTemp = []
+        w = w.strip()
+        wordTemp.append(w.split(";")[0])
+        wordTemp.append(w.split(";")[1].split(","))
+        wordStorage.append(wordTemp)
+    return wordStorage
+
 
 def getWordByTags(wordListFilter, *tagFilterList):
-  randomWord = ""
-  matchingWordList = []
-  exclusiveTagsFilterList = []
-  inclusiveTagsFilterList = []
-  for t in tagFilterList:
-    t = str(t)
-    if (t != "None"):
-      if t in exclusiveTags:
-        exclusiveTagsFilterList.append(t)
-        # print("exclusive tag: " + t)
-      else:
-        inclusiveTagsFilterList.append(t)
-        # print("regular tag: " + t)
-    else:
-      getWordByTags(wordListFilter, tagFilterList)
-  for l in wordListFilter:
-    if all(search in l[1] for search in inclusiveTagsFilterList):
-      # matchingWordList.append(l[0])
-      if all(search not in l[1] for search in exclusiveTagsFilterList):
-        matchingWordList.append(l[0])
-  if len(matchingWordList) > 0:
-    randomWord = random.choice(matchingWordList)
-  return randomWord
+    randomWord = ""
+    matchingWordList = []
+    exclusiveTagsFilterList = []
+    inclusiveTagsFilterList = []
+    for t in tagFilterList:
+        t = str(t)
+        if t != "None":
+            if t in exclusiveTags:
+                exclusiveTagsFilterList.append(t)
+                # print("exclusive tag: " + t)
+            else:
+                inclusiveTagsFilterList.append(t)
+                # print("regular tag: " + t)
+        else:
+            getWordByTags(wordListFilter, tagFilterList)
+    for l in wordListFilter:
+        if all(search in l[1] for search in inclusiveTagsFilterList):
+            # matchingWordList.append(l[0])
+            if all(search not in l[1] for search in exclusiveTagsFilterList):
+                matchingWordList.append(l[0])
+    if len(matchingWordList) > 0:
+        randomWord = random.choice(matchingWordList)
+    return randomWord
+
 
 def getTags(wordToCheck, wordsList):
-  wordIdx = [i for i, ele in wordsList].index(wordToCheck)
-  # print("wordIdx: ")
-  # print(wordIdx)
-  return wordsList[wordIdx][1]
+    wordIdx = [i for i, ele in wordsList].index(wordToCheck)
+    # print("wordIdx: ")
+    # print(wordIdx)
+    return wordsList[wordIdx][1]
+
 
 def getWordWithoutTags(wordListFilter, *tagFilterList):
-  returnList = []
-  for l in wordListFilter:
-    if all(search in l[1] for search in tagFilterList):
-      returnList.append(l[0])
-  randomWord = random.choice(returnList)
-  return randomWord
+    returnList = []
+    for l in wordListFilter:
+        if all(search in l[1] for search in tagFilterList):
+            returnList.append(l[0])
+    randomWord = random.choice(returnList)
+    return randomWord
+
 
 def randColor():
     color = "%06x" % random.randint(0, 0xFFFFFF)
     return str(color)
 
+
 def getUnsplashPhoto(search_term, w=640, h=480):
-  # instantiate pyunsplash connection object
-  api = PyUnsplash(api_key=client_id)
+    # instantiate pyunsplash connection object
+    api = PyUnsplash(api_key=client_id)
 
-  print(search_term.strip("-"))
+    print(search_term.strip("-"))
 
-  try:
-    # Retrieve random photo matching search term from unsplash
-    unsplash_photo_coll = api.photos(type_='random', count=1, query=search_term.strip("-"))
+    try:
+        # Retrieve random photo matching search term from unsplash
+        unsplash_photo_coll = api.photos(
+            type_="random", count=1, query=search_term.strip("-")
+        )
 
-    # retrieve raw url of photo
-    unsplash_photo = next(unsplash_photo_coll.entries).body['urls']['raw'] + "&w=" + str(w) + "&h=" + str(h)
-    bg_image = r'url("' + unsplash_photo + r'")'
-  except:
-    bg_image = "linear-gradient(" + str(random.randint(0, 180)) + "deg, #" + randColor() + ", #" + randColor() + ")"
+        # retrieve raw url of photo
+        unsplash_photo = (
+            next(unsplash_photo_coll.entries).body["urls"]["raw"]
+            + "&w="
+            + str(w)
+            + "&h="
+            + str(h)
+        )
+        bg_image = r'url("' + unsplash_photo + r'")'
+    except:
+        bg_image = (
+            "linear-gradient("
+            + str(random.randint(0, 180))
+            + "deg, #"
+            + randColor()
+            + ", #"
+            + randColor()
+            + ")"
+        )
 
-  return bg_image
+    return bg_image
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-  print("-----new genre-----")
-  wordsList = processWordList(wordsCombined)
-  phrase = []
-  phraseTags = []
-  unsplash_search_eligible = []
-  finalPhrase = ""
-  for phraseIdx in range(phraseLength):
-    pickedWord = getWordByTags(wordsList, phraseIdx)
-    pickedWordTags = getTags(pickedWord, wordsList)
-    # check to see if word can be photo searched
-    if 'p' in pickedWordTags:
-      unsplash_search_eligible.append(pickedWord)
 
-    for pt in (pickedWordTags):      
-      if not pt.isdecimal():
-        # not appending tags indicating position
-        # which are numeric
-        phraseTags.append(pt)
-    if (phraseIdx == 0):
-    # If index is zero we are getting adjective 1 and not using sequence matcher
-      phrase.append(pickedWord)
+@app.route("/", defaults={"share_uuid": None})
+@app.route("/<share_uuid>")
+def index(share_uuid):
+    print("-----new genre-----")
+    wordsList = processWordList(wordsCombined)
+    phrase = []
+    phraseTags = []
+    unsplash_search_eligible = []
+    phrase_result = ""
+    for phraseIdx in range(phraseLength):
+        pickedWord = getWordByTags(wordsList, phraseIdx)
+        pickedWordTags = getTags(pickedWord, wordsList)
+        # check to see if word can be photo searched
+        if "p" in pickedWordTags:
+            unsplash_search_eligible.append(pickedWord)
+
+        for pt in pickedWordTags:
+            if not pt.isdecimal():
+                # not appending tags indicating position
+                # which are numeric
+                phraseTags.append(pt)
+        if phraseIdx == 0:
+            # If index is zero we are getting adjective 1 and not using sequence matcher
+            phrase.append(pickedWord)
+        else:
+            # if we aren't on the first word in the phrase...
+            # compare current word with previous word in phrase
+            s = SequenceMatcher(None, phrase[phraseIdx - 1], pickedWord)
+            while s.ratio() > 0.8:
+                print("Word too similar:")
+                print(pickedWord)
+                pickedWord = getWordByTags(wordsList, str(phraseIdx), phraseTags)
+                s = SequenceMatcher(None, phrase[phraseIdx - 1], pickedWord)
+            phrase.append(pickedWord)
+
+    # Pick background photo
+    print("searchable words: ")
+    print(unsplash_search_eligible)
+
+    if len(unsplash_search_eligible) > 0:
+        search_term = random.choice(unsplash_search_eligible)
+        print("searching for " + search_term)
+        bg_image = getUnsplashPhoto(search_term)
     else:
-    # if we aren't on the first word in the phrase...
-    # compare current word with previous word in phrase
-      s = SequenceMatcher(None, phrase[phraseIdx-1], pickedWord)
-      while s.ratio() > 0.8:
-        print("Word too similar:")
-        print(pickedWord)
-        pickedWord = getWordByTags(wordsList, str(phraseIdx), phraseTags)
-        s = SequenceMatcher(None, phrase[phraseIdx-1], pickedWord)
-      phrase.append(pickedWord)
+        print(
+            "default search for ocean picture, none of the random terms look like they would work"
+        )
+        search_term = "ocean"
+        bg_image = getUnsplashPhoto("ocean")
 
-  # Pick background photo 
-  print("searchable words: ")
-  print(unsplash_search_eligible)
+    print(bg_image)
+    # Join dashed words together
+    for phraseIdx in range(phraseLength):
+        if not (phrase[phraseIdx].endswith("-")):
+            phrase_result += phrase[phraseIdx] + " "
+        else:
+            phrase_result += phrase[phraseIdx]
 
-  if len(unsplash_search_eligible) > 0:
-    search_term = random.choice(unsplash_search_eligible)
-    print("searching for " + search_term)
-    bg_image = getUnsplashPhoto(search_term)
-  else:
-    print("default search for ocean picture, none of the random terms look like they would work")
-    search_term = "ocean"
-    bg_image = getUnsplashPhoto('ocean')
+    print(phrase_result)
 
-  print(bg_image)
-  # Join dashed words together
-  for phraseIdx in range(phraseLength):
-    if not(phrase[phraseIdx].endswith('-')):
-      finalPhrase += phrase[phraseIdx] + " "
+    new_shared_url = UrlShare(phrase_result, bg_image, search_term)
+    share_redir = "/" + new_shared_url.uuid
+
+    if share_uuid == None:
+        new_shared_url.commit()
+        return redirect(share_redir)
     else:
-      finalPhrase += phrase[phraseIdx]
+        # return str(new_shared_url.phrase)
+        return render_template(
+            "index.html",
+            phrase_result=new_shared_url.phrase,
+            bg_image=new_shared_url.img_url,
+            search_term=new_shared_url.search_term,
+        )
 
-  print(finalPhrase)
-
-  # abandoned this method of searching for picture
-  # bg_search_term = max(finalPhrase.split(" "), key=len)
-  # print(bg_search_term)
-
-  return render_template("index.html", result=finalPhrase, bg_image=bg_image, search_term=search_term)
-
-def save_url(phrase=None, img=None, search=None):
-  new_shared_url = UrlShare(phrase, img, search)
-  new_shared_url.commit()
-
-@app.route("/share", defaults={'share_uuid': None})
-@app.route("/share/<share_uuid>") # , methods=['GET', 'POST'])
-def share(share_uuid):
-  # Initialize Firebase connection
-  db = firestore.Client(project='genremachine-2e8a4')
-  shared_genre = db.collection(u'shared_urls').document(share_uuid)
-
-  if share_uuid == None:
-    save_url(phrase=None, img=None, search=None)
-  sg = shared_genre.get()
-  if sg.exists:
-      print(sg.to_dict())
-      return sg.to_dict()
-  else:
-      print(u'No such document!')
-      return "No such document!"
 
 if __name__ == "__main__":
     app.run()
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger = logging.getLogger("gunicorn.error")
